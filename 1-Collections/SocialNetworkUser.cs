571d3f7f2215c04df444f92ed4d43baa39090878
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2022-12-14 15:48:33 +0100

    Completed and corrected exercise 1

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -6,27 +6,52 @@ namespace Collections
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
+        IDictionary<string, List<TUser>> _followedUsers;
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
+            // throw new NotImplementedException("TODO is there anything to do here?");
+            _followedUsers = new Dictionary<string, List<TUser>>();
         }
 
         public bool AddFollowedUser(string group, TUser user)
         {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
+            // throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
+            if (_followedUsers.ContainsKey(group))
+            {
+                _followedUsers[group].Add(user);
+                return false;
+            }
+            else
+            {
+                _followedUsers.Add(group, new List<TUser> {user});
+                return true;
+            }
         }
 
         public IList<TUser> FollowedUsers
         {
             get
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                // throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                var allUsers = new List<TUser>();
+                foreach (var key in _followedUsers.Keys)
+                {
+                    allUsers.AddRange(_followedUsers[key].AsReadOnly());
+                }
+                return allUsers;
             }
         }
 
         public ICollection<TUser> GetFollowedUsersInGroup(string group)
         {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
+            if (_followedUsers.ContainsKey(group))
+            {
+                return _followedUsers[group].AsReadOnly();
+            }
+            else
+            {
+                return new List<TUser>();
+            }
         }
     }
 }
--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -6,8 +6,15 @@ namespace Collections
     {
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            // throw new NotImplementedException("TODO ensure that username is not null");
+            // throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            if (fullName == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Age = age;
+            FullName = fullName;
+            Username = username; 
         }
         
         public uint? Age { get; }
@@ -16,8 +23,12 @@ namespace Collections
         
         public string Username { get; }
 
-        public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
+        public bool IsAgeDefined => Age.HasValue;
         
         // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
+        
+        public override string ToString() => $"Name = {FullName}, Username = {Username}, Age = {Age}";
+
+        public override int GetHashCode() => HashCode.Combine(FullName, Username, Age); 
     }
 }
