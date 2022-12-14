b39bbe99edf99c7abc4b4bc10a03b7225b5681ee
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2022-12-14 15:48:55 +0100

    Completed and corrected exercise 2

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -20,6 +20,10 @@ namespace Exceptions
         public object GetFirst()
         {
             // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (Count == 0)
+            {
+                throw new EmptyQueueException();
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
@@ -28,6 +32,10 @@ namespace Exceptions
         public void AddLast(object item)
         {
             // TODO ensure objects can only be inserted if the item is queue is not full
+            if (Count == Capacity)
+            {
+                throw new FullQueueException();
+            }
             _items[_lastIndex % Capacity] = item;
             _lastIndex++;
         }
